94f9109ee335b8b44fc2d4f0538310bc3e602bc8
Alex Yesmanchyk <esmanchik@gmail.com>
2024-04-12 12:35:28 -0400

    Used boost::ut in the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "alpha.h"
 #include <vector>
 #include <string>
+#include <boost/ut.hpp>
 
 int main() {
     alpha();
-
+    
     std::vector<std::string> vec;
     vec.push_back("test_package");
-
+    
     alpha_print_vector(vec);
+
+    boost::ut::expect(true);
 }
